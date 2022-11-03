6354e5312ce55ac705bdef7d5cfbfa8bd98ac9d4
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2022-11-03 10:51:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BendingVisualizer
+ A Project Korra addon Plugin to allow the visualizing of moves and combos with NPCS
---
+++ src/main/java/me/justahuman/bendingvisualizer/bendingvisualizer/BendingVisualizer.java
@@ -0,0 +1,17 @@
+package me.justahuman.bendingvisualizer.bendingvisualizer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BendingVisualizer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

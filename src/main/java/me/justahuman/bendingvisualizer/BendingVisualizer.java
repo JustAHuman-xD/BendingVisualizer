8551da53aecd2cf2d35d56930fb41ae63a002ba9
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2022-11-04 12:35:21 -0500

    Prepare Config and Command

--- src/main/java/me/justahuman/bendingvisualizer/BendingVisualizer.java
+++ src/main/java/me/justahuman/bendingvisualizer/BendingVisualizer.java
@@ -1,17 +1,24 @@
 package me.justahuman.bendingvisualizer;
 
+import me.justahuman.bendingvisualizer.managers.ConfigManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BendingVisualizer extends JavaPlugin {
+    private static BendingVisualizer plugin;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        plugin = this;
+        ConfigManager.initialize();
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+       ConfigManager.save();
+    }
+
+    public static BendingVisualizer getPlugin() {
+        return plugin;
     }
 }
